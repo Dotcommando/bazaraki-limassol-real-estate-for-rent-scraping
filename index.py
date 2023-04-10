7155e821a37cdba9ce70c159bd35f7b5216137f8
Mikhail Filchushkin <webestet@gmail.com>
2023-04-10 22:34:23 +0300

    Two types of URLs available for scraping, named and with set coords

--- index.py
+++ index.py
@@ -8,6 +8,7 @@ from dotenv import load_dotenv
 import Levenshtein
 import re
 import random
+import sys
 
 load_dotenv()
 
@@ -28,6 +29,10 @@ def get_response(url):
         print(f"Error: HTTP status code {response.status_code} for URL {url}")
     return response
 
+def print_progress(current, total, city):
+    sys.stdout.write("\r\nScraped {} of {} ads in {}".format(current, total, city))
+    sys.stdout.flush()
+
 def parse_date(date_str):
     if "Yesterday" in date_str:
         date_str = date_str.replace("Yesterday", (datetime.now() - timedelta(days=1)).strftime("%d.%m.%Y"))
@@ -35,16 +40,25 @@ def parse_date(date_str):
         date_str = date_str.replace("Today", datetime.now().strftime("%d.%m.%Y"))
     return int(time.mktime(datetime.strptime(date_str.strip(), "%d.%m.%Y %H:%M").timetuple()))
 
-def get_page_links(soup):
-    max_page_number = 1
-    pagination = soup.find("ul", class_="number-list")
+def get_page_links(soup, lat=None, lng=None, radius=None):
+    page_links = []
+    pagination = soup.find("div", class_="pagination")
+
     if pagination:
-        for li in pagination.find_all("li"):
-            link = li.find("a", class_="page-number")
-            if link:
-                page_number = int(link["data-page"])
-                max_page_number = max(max_page_number, page_number)
-    return [f"/real-estate-to-rent/apartments-flats/?lat=34.69708393413313&lng=33.01509693679961&radius=15000&page={i}" for i in range(1, max_page_number + 1)]
+        last_page = int(pagination.find_all("a")[-2].text)
+
+        for i in range(2, last_page + 1):
+            if lat and lng and radius:
+                link = f"/real-estate-to-rent/apartments-flats/?page={i}&lat={lat}&lng={lng}&radius={radius}"
+            else:
+                url_with_page_param = soup.find("a", href=True, text=str(i))
+                if url_with_page_param:
+                    link = url_with_page_param["href"]
+                else:
+                    break
+            page_links.append(link)
+
+    return page_links
 
 def parse_announcement(soup):
     id_tag = soup.find("a", class_="announcement-block__title")
@@ -94,14 +108,15 @@ def parse_announcement(soup):
         "ad_id": ad_id
     }
 
-def scrape_announcements(url):
+def scrape_announcements(url, parsed_urls):
     response = get_response(url)
     soup = BeautifulSoup(response.text, "html.parser")
     announcements = []
     for announcement_soup in soup.find_all("li", class_="announcement-container"):
         announcement = parse_announcement(announcement_soup)
-        if announcement:
+        if announcement and announcement['url'] not in parsed_urls:
             announcements.append(announcement)
+            parsed_urls.add(announcement['url'])
     return announcements
 
 def get_city_regions():
@@ -151,20 +166,28 @@ def extract_additional_data(url):
     random_delay(MIN_DELAY, MAX_DELAY)
     return data
 
-def main(city, city_url, filename):
+def main(city, regions, filename, parsed_urls):
     all_announcements = []
 
-    start_url = BASE_URL + city_url
+    for region, city_url in regions.items():
+        print(f"Scraping {city} {region}")
 
-    response = get_response(start_url)
-    soup = BeautifulSoup(response.text, "html.parser")
+        start_url = BASE_URL + city_url
 
-    page_links = get_page_links(soup)
-    all_announcements += scrape_announcements(start_url)
+        response = get_response(start_url)
+        soup = BeautifulSoup(response.text, "html.parser")
 
-    for link in page_links:
-        url = BASE_URL + link
-        all_announcements += scrape_announcements(url)
+        lat_tag = soup.find("input", id="lat")
+        lng_tag = soup.find("input", id="lng")
+        radius_tag = soup.find("input", id="radius")
+
+        page_links = get_page_links(soup, lat_tag["value"] if lat_tag else None, lng_tag["value"] if lng_tag else None, radius_tag["value"] if radius_tag else None)
+
+        all_announcements += scrape_announcements(start_url, parsed_urls)
+
+        for link in page_links:
+            url = BASE_URL + link
+            all_announcements += scrape_announcements(url, parsed_urls)
 
     announcements_df = pd.DataFrame(all_announcements)
 
@@ -176,22 +199,27 @@ def main(city, city_url, filename):
 
     combined_df.to_csv(filename, index=False)
 
-    for index, row in announcements_df.iterrows():
-        additional_data = extract_additional_data(row['url'])
+    visited_ads = 0
+    ads_to_save = 5
+    total_ads = len(combined_df)
 
-        for column_name, value in additional_data.items():
-            if column_name not in combined_df.columns:
-                combined_df[column_name] = None
-            combined_df.at[index, column_name] = value
+    for index, announcement in combined_df.iterrows():
+        if "area" not in announcement or pd.isnull(announcement["area"]):
+            url = announcement["url"]
+            additional_data = extract_additional_data(url)
+            for key, value in additional_data.items():
+                combined_df.at[index, key] = value
 
-        if (index + 1) % 5 == 0:
-            combined_df.to_csv(filename, index=False)
+            visited_ads += 1
+            if visited_ads % ads_to_save == 0:
+                combined_df.to_csv(filename, index=False)
+                print_progress(visited_ads, total_ads, city)
 
     combined_df.to_csv(filename, index=False)
 
 if __name__ == "__main__":
+    parsed_urls = set()
     city_regions = get_city_regions()
     for city, regions in city_regions.items():
         filename = f"parsed_data/{city.lower()}_{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.csv"
-        for region, region_url in regions.items():
-            main(city, region_url, filename)
+        main(city, regions, filename, parsed_urls)
